803e5bb10f5b2ca737fdcd4e992f4801da73bc8e
Julian Aburrow <vdub1992@hotmail.co.uk>
2026-09-19 14:50:25 +0100

    Conflict resolution

--- FilmReferenceUI/Components/Pages/Home.razor.cs
+++ FilmReferenceUI/Components/Pages/Home.razor.cs
@@ -45,7 +45,6 @@ public partial class Home
         FeaturedPerson = await PersonHandler.GetFeaturedPersonAsync();
         FeaturedPersonToShow = FeaturedPerson is not null && FeaturedPerson.PersonId != 0;
 
-
         StateHasChanged();
     }
 
